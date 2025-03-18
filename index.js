6a04c8337f9281569f20eeab3fa2163495163d15
Trevor Karuma <trevormukundi@gmail.com>
2025-03-18 21:05:53 +0300

    adds inde.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+document.querySelector("main").remove();
+
+
+let newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+newHeader.textContent = "TREVOR KARUMA  is the champion";
+
+
+document.body.append(newHeader);
